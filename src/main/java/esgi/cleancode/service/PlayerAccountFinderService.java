b11bb21f52a053fdce448661816d27d8ff875891
Barlords <basile.pulin@gmail.com>
2022-12-29 17:21:27 +0100

    PlayerAccountFinderService OK

--- src/main/java/esgi/cleancode/service/PlayerAccountFinderService.java
+++ src/main/java/esgi/cleancode/service/PlayerAccountFinderService.java
@@ -1,13 +1,19 @@
 package esgi.cleancode.service;
 
+import esgi.cleancode.database.InMemoryDatabase;
 import esgi.cleancode.domain.PlayerAccount;
+import esgi.cleancode.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 public class PlayerAccountFinderService {
+
+    private final InMemoryDatabase database;
 
     public PlayerAccount findById(UUID id)
     {
-        return null;
+        return database.findPlayerAccountById(id).orElseThrow(() -> new ResourceNotFoundException("PlayerAccount not found"));
     }
 }
